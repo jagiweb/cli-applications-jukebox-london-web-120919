943442b7b0b6618d26f9782cfa3cfd718ebeadb3
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-07 03:34:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,9 +1,9 @@
 # Add your code here
 def help
-  puts "I accept the following commands:/n
-- help : displays this help message/n
-- list : displays a list of songs you can play/n
-- play : lets you choose a song to play/n
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
 - exit : exits this program"
 end
 
