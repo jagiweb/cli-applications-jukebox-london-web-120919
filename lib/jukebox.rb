05dd1788260463a144f84963e77f3df76e2f33a2
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-07 03:30:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,10 @@
 # Add your code here
 def help
+  puts "I accept the following commands:/n
+- help : displays this help message/n
+- list : displays a list of songs you can play/n
+- play : lets you choose a song to play/n
+- exit : exits this program"
 end
 
 def play
@@ -9,5 +14,9 @@ def list
 end
 
 def exit_jukebox
+
+end
+
+def run
 
 end
